8eee6d7b59389903272f7683cbbf99f76685b2cd
OCD0711 <conghaohu0711@outlook.com>
2021-08-24 18:35:13 +0800

    remove futile setting/opcamera

--- common.mk
+++ common.mk
@@ -23,8 +23,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sm8250-common/sm8250-common-vendor.mk)
 
-# Inherit packages from vendor/oneplus/addons/camera
-$(call inherit-product, vendor/oneplus/addons/camera/camera-vendor.mk)
+# Inherit packages from vendor/oneplus/apps/sm8250-common/config.mk 
+# $(call inherit-product, vendor/oneplus/apps/sm8250-common/config.mk )
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
